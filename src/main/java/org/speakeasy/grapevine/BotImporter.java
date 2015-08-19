8493c283d15f5d5d63fecb0adcd566ed15299829
SpeaKeasY <speakeasysky@gmail.com>
2015-08-19 06:04:33 -0400

    Made file processing faster.

--- src/main/java/org/speakeasy/grapevine/BotImporter.java
+++ src/main/java/org/speakeasy/grapevine/BotImporter.java
@@ -57,36 +57,24 @@ public class BotImporter {
         String line = null;
         Bird bird = new Bird();
         while (i <= j) {
-            line = fileLines.get(i);
+            line = fileLines.get(i++);
             if (line.startsWith("OAUTH_TOKEN")) {
                 bird.setOAuthToken(line.substring(12, line.length() + 1));
-            } else if (line.startsWith("OAUTH_SECRET")) {
+                line = fileLines.get(i++);
                 bird.setOAuthSecret(line.substring(13, line.length() + 1));
-            } else if (line.startsWith("CONSUMER_KEY")) {
+                line = fileLines.get(i++);
                 bird.setConsumerToken(line.substring(13, line.length() + 1));
-            } else if (line.startsWith("CONSUMER_SECRET")) {
+                line = fileLines.get(i++);
                 bird.setConsumerSecret(line.substring(16, line.length() + 1));
-            } else if (line.startsWith("TWITTER_HANDLE")) {
+                line = fileLines.get(i++);
                 bird.setName(line.substring(15, line.length() + 1));
-            } else if (line.startsWith("ALREADY_FOLLOWED_FILE")) {
-                // Already followed file import...
-            } else if (line.startsWith("FOLLOWERS_FILE")) {
-                // Followers import...
-            } else if (line.startsWith("FOLLOWS_FILE")) {
-                // Follows import...
-            } else if (line.startsWith("USERS_KEEP_FOLLOWING")) {
+                line = fileLines.get(i+3); // 3
                 bird.setFollowGroup(toUserMap(line.substring(21, line.length() + 1)));
-            } else if (line.startsWith("USERS_KEEP_UNMUTED")) {
+                line = fileLines.get(i++);
                 bird.setUsersKeepUnMuted(toUserMap(line.substring(19, line.length() + 1)));
-            } else if (line.startsWith("USERS_KEEP_MUTED")) {
+                line = fileLines.get(i++);
                 bird.setUsersMuted(toUserMap(line.substring(17, line.length() + 1)));
-            } else if (line.startsWith("FOLLOW_BACKOFF_MIN_SECONDS")) {
-                // NA
-            } else if (line.startsWith("FOLLOW_BACKOFF_MAX_SECONDS")) {
-                // NA
             }
-            // Ingore the rest and incriment.
-            i++;
         }
     }
 
